217952ddd2d1bdbcf6640a0f8e6307d7792a87d4
aiocat <aiocat@protonmail.com>
2022-04-20 01:43:21 +0300

    add notification builder

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,88 @@
 // Copyright (c) 2022 aiocat
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
-console.log("works?");
+var Totify = /** @class */ (function () {
+    function Totify() {
+    }
+    /**
+     * Initialize totify settings (if don't exists)
+     *
+     * @remarks
+     * This method is part of the {@link core-library#Totify | Totify subsystem}.
+     *
+     */
+    Totify.init = function (posX, posY) {
+        var notificationDiv = document.getElementById("TOTIFY_NOTIFICATIONS");
+        if (notificationDiv)
+            return;
+        notificationDiv = document.createElement("div");
+        notificationDiv.style.position = "fixed";
+        notificationDiv.style.width = "312px";
+        notificationDiv.style.height = "auto";
+        notificationDiv.style.zIndex = "1337";
+        notificationDiv.id = "TOTIFY_NOTIFICATIONS";
+        switch (posX) {
+            case "RIGHT":
+                notificationDiv.style.right = "0px";
+                break;
+            case "LEFT":
+                notificationDiv.style.left = "0px";
+                break;
+        }
+        switch (posY) {
+            case "TOP":
+                notificationDiv.style.top = "0px";
+                break;
+            case "BOTTOM":
+                notificationDiv.style.bottom = "0px";
+                break;
+        }
+        document.body.appendChild(notificationDiv);
+    };
+    /**
+     * Notification builder for Totify.
+     *
+     * @remarks
+     * This method is part of the {@link core-library#Totify | Totify subsystem}.
+     *
+     * @param type - Notification type
+     * @param text - Notification content
+     *
+     */
+    Totify.build = function (type, text) {
+        var notificationDiv = document.getElementById("TOTIFY_NOTIFICATIONS");
+        if (!notificationDiv)
+            return;
+        var notification = document.createElement("div");
+        notification.innerText = text;
+        notification.style.padding = "5px";
+        notification.style.margin = "5px";
+        notification.style.borderRadius = "5px";
+        notification.style.cursor = "pointer";
+
+        notification.onclick = function () { return notification.remove(); };
+        switch (type) {
+            case "INFO":
+                notification.style.background = "#3498db";
+                notification.style.borderBottom = "2px solid #2084c7";
+                break;
+            case "ERROR":
+                notification.style.background = "#e74c3c";
+                notification.style.borderBottom = "2px solid #d33828";
+                break;
+            case "SUCCESS":
+                notification.style.background = "#2ecc71";
+                notification.style.borderBottom = "2px solid #1ab85c";
+                break;
+            case "WARN":
+                notification.style.background = "#f39c12";
+                notification.style.borderBottom = "2px solid #cf8611";
+                break;
+        }
+        notificationDiv.appendChild(notification);
+    };
+    return Totify;
+}());
+export { Totify };
 //# sourceMappingURL=index.js.map
---
+++ src/types.ts
@@ -0,0 +1,22 @@
+// Copyright (c) 2022 aiocat
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/**
+ * Notification type for totify builder
+ *
+ */
+export type NotificationType = "WARN" | "INFO" | "ERROR" | "SUCCESS";
+
+/**
+ * Notification X position for totify container
+ *
+ */
+export type PositionX = "LEFT" | "RIGHT";
+
+/**
+ * Notification Y position for totify container
+ *
+ */
+export type PositionY = "TOP" | "BOTTOM";
